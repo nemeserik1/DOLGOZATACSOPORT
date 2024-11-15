dba931dbc8eb3a74fb9f5fbdfb073fd2ed0a3b96
nemeserik1 <nemes.erik@nejanet.hu>
2024-11-15 09:49:39 +0100

    asd
    asd

--- dolgozatA/routes/web.php
+++ dolgozatA/routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/new-genre', [TopicController::class, 'index'])->name('topics.index');
+Route::post('/new-genre', [TopicController::class, 'store'])->name('topics.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
