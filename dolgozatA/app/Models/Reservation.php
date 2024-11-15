0aa845e673560bfcef573f6fd92c685c2b8a4dd5
nemeserik1 <nemes.erik@nejanet.hu>
2024-11-15 09:00:32 +0100

    fasz

--- dolgozatA/app/Models/Reservation.php
+++ dolgozatA/app/Models/Reservation.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['reservation_email','reservation_time', 'reservation_backtime'];
 }
