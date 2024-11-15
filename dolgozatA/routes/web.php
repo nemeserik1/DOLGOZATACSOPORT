27f9884d860dadaa56f905195dbcfbb50ff9ea3f
nemeserik1 <nemes.erik@nejanet.hu>
2024-11-15 09:39:41 +0100

    asd
    asd

--- dolgozatA/routes/web.php
+++ dolgozatA/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new-genre', [TopicController::class, 'index'])->name('topics.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
