ddc17a804f425d450f73c321a7253cacd936a8e3
nemeserik1 <nemes.erik@nejanet.hu>
2024-11-15 09:57:51 +0100

    asdasd
    asdsd

--- dolgozatA/app/Http/Controllers/BookController.php
+++ dolgozatA/app/Http/Controllers/BookController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Topic;
+use App\Models\Book;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        return view('topics.genre');
+    }
 }
